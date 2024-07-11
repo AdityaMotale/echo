bde4b5253845d1905dafd91df5cfdf78dd2bda16
Adii <77727680+AdityaMotale@users.noreply.github.com>
2024-07-11 14:17:35 +0530

    completed chat menu UI

---
+++ src/routes/chat/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import Menu from './Menu.svelte';
+</script>
+
+<section class="w-full h-full flex p-4 gap-4 relative">
+	<Menu />
+	<div class="w-full"></div>
+</section>
---
+++ src/routes/chat/Menu.svelte
@@ -0,0 +1,68 @@
+<script lang="ts">
+	import MenuIcon from '$lib/icons/menu.svg';
+	import CloseIcon from '$lib/icons/close.svg';
+	import NewChatIcon from '$lib/icons/new_chat.svg';
+	import LogoutIcon from '$lib/icons/logout.svg';
+	import TrashIcon from '$lib/icons/trash.svg';
+
+	let isMenuOpen = true;
+
+	let chats: string[] = ['Session#01', 'Session#02', 'Session#03', 'Session#04', 'Session#05'];
+</script>
+
+{#if isMenuOpen}
+	<div
+		class="w-screen h-full max-w-screen md:max-w-80 bg-secBg rounded-xl
+            md:static absolute z-50 top-0 left-0
+            flex flex-col p-4"
+	>
+		<!-- Header -->
+		<div class="w-full flex items-center justify-start gap-2">
+			<h2 class="font-display font-semibold text-2xl">Chats</h2>
+			<div class="grow" />
+			<button class="bg-bg p-1.5 rounded-lg" on:click={() => {}}>
+				<img src={NewChatIcon} alt="New Chat Icon" width="20px" height="20px" />
+			</button>
+			<button
+				class="bg-bg p-1 rounded-lg"
+				on:click={() => {
+					isMenuOpen = !isMenuOpen;
+				}}
+			>
+				<img src={CloseIcon} alt="Close Icon" width="24px" height="24px" />
+			</button>
+		</div>
+
+		<!-- Chats -->
+		<section class="grow overflow-y-scroll my-7">
+			<div class="flex flex-col gap-2">
+				{#each chats as chat}
+					<button class="hover:bg-bg flex items-center justify-between p-2 rounded-lg w-full">
+						<p class="font-sans font-medium text-lg">{chat}</p>
+						<button class="bg-bg hover:bg-secBg p-1.5 rounded-lg">
+							<img src={TrashIcon} alt="Trash Icon" width="20px" height="20px" />
+						</button>
+					</button>
+				{/each}
+			</div>
+		</section>
+
+		<!-- Logout -->
+		<button class="bg-bg flex items-center gap-2 p-2 rounded-lg">
+			<div class="bg-bg p-1.5 rounded-lg">
+				<img src={LogoutIcon} alt="Logout Icon" width="20px" height="20px" />
+			</div>
+			<p class="font-sans font-medium text-lg">Logout</p>
+		</button>
+	</div>
+{:else}
+	<!-- Menu Button for when menu is closed -->
+	<button
+		class="bg-secBg p-2 rounded-lg absolute"
+		on:click={() => {
+			isMenuOpen = !isMenuOpen;
+		}}
+	>
+		<img src={MenuIcon} alt="Right Arrow Icon" width="24px" height="24px" />
+	</button>
+{/if}
