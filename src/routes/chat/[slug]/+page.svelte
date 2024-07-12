92f29f517f1bc4a5e40a70fb802a59bb96a0ba8f
Adii <77727680+AdityaMotale@users.noreply.github.com>
2024-07-12 21:21:57 +0530

    ready for test deployment

--- src/routes/chat/[slug]/+page.svelte
+++ src/routes/chat/[slug]/+page.svelte
@@ -2,19 +2,31 @@
 	import Chats from '../Chats.svelte';
 	import Menu from '../Menu.svelte';
 	import { createChatDetailsList } from '$lib/core/stores';
-	import { onDestroy, onMount } from 'svelte';
+	// import { onMount } from 'svelte';
+	// import io, { Socket } from 'socket.io-client';
 
 	export let data: PageData;
 
-	onMount(() => {
-		console.log('New CMP!');
-	});
+	// let socket: Socket;
 
 	let chatDetailsList = createChatDetailsList(data.id);
 
 	function handleMessages(event: any) {
+		// socket.emit('message', event.detail);
 		chatDetailsList.add(event.detail);
 	}
+
+	// onMount(() => {
+	// 	socket = io('https://echo-websocket.vercel.app/');
+
+	// 	socket.on('connect', () => {
+	// 		console.log('Connected to WebSocket server');
+	// 	});
+
+	// 	socket.on('message', (message) => {
+	// 		console.log({ message });
+	// 	});
+	// });
 </script>
 
 <svelte:head>
